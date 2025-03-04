de7b0a25ac40dc3881c3551d775618e365907bf8
devchuy <93514768+IAmChuy@users.noreply.github.com>
2025-03-04 15:00:21 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Shapescape Easy Glyphs'
 copyright = '2025, Shapescape'
 author = 'Shapescape'
-release = '0.1.0'
+release = '1.0.1'
 
 # -- General configuration
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
